ee0436ceec49e975b53dc9dae30c40f0af3c854e
Diorbert Pereira <diorbert@gmail.com>
2022-04-25 18:36:05 -0300

    Including utility functions inside InputRelationship for derived characteristics

--- src/libs/model/input_descriptor/input_relationship.class.ts
+++ src/libs/model/input_descriptor/input_relationship.class.ts
@@ -1,6 +1,12 @@
 import {RelationshipDiscriminator} from "@libs/model/input_descriptor/enums/relationship_discriminator.enum";
 import {ConnectorDiscriminator} from "@libs/model/input_descriptor/enums/connector_discriminator.enum";
 
+const derivedDiscriminators = [
+    ConnectorDiscriminator.PATH_LEFT,
+    ConnectorDiscriminator.PATH_RIGHT,
+    ConnectorDiscriminator.PATH_BASE
+];
+
 export class InputRelationship {
     constructor(
         protected _discriminator: RelationshipDiscriminator,
@@ -9,7 +15,8 @@ export class InputRelationship {
         protected _alias: string,
         protected _types: Array<string>,
         protected _isNegated: boolean
-    ) {}
+    ) {
+    }
 
     get discriminator(): RelationshipDiscriminator {
         return this._discriminator;
@@ -47,11 +54,33 @@ export class InputRelationship {
         return this._isNegated;
     }
 
+    get isBidirectional(): boolean {
+        return this.getDirectionAsNumber() === 0;
+    }
+
+    get isSourceDerived(): boolean {
+        return derivedDiscriminators.includes(this._sourceDisc);
+    }
+
+    get isTargetDerived(): boolean {
+        return derivedDiscriminators.includes(this._targetDisc);
+    }
+
+    /**
+     * Indicates that the relationship has both connectors of the same nature (PATH or BONDED)
+     *
+     * @return true if source discriminator and target discriminator are both classified as PATH or BONDED
+     */
+    get isHomogeneous(): boolean {
+        return (this.isSourceDerived && this.isTargetDerived) || (!this.isSourceDerived && !this.isTargetDerived);
+    }
+
     /**
      * Provides the direction as a number from -1 to 1
+     *
      * @return -1 if the relationship points to the left, 0 if bidirectional and 1 if the relationship points to the right
      */
-    getDirectionAsNumber () :number {
+    getDirectionAsNumber(): number {
         if ((this.sourceDisc === 'BONDED_BASE' && this.targetDisc === 'BONDED_RIGHT') ||
             (this.sourceDisc === 'PATH_BASE' && this.targetDisc === 'PATH_RIGHT')) {
             return 1;
