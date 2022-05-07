315cdbf60ce43f297bf682c4dc47a742c9ea83cd
Diorbert Pereira <diorbert@gmail.com>
2022-05-07 17:24:59 -0300

    Organizing tests in files

--- __tests__/unit/utils/validateQueryDescriptor.ts
+++ __tests__/unit/utils/validateQueryDescriptor.ts
@@ -1,5 +1,7 @@
 import {InputDescriptor} from "../../../src/libs/model/input_descriptor/input_descriptor.class";
 import {QueryDescriptor} from "../../../src/libs/model/query_descriptor/query_descriptor.class";
+import {InputNode} from "../../../src/libs/model/input_descriptor/input_node.class";
+import {InputRelationship} from "../../../src/libs/model/input_descriptor/input_relationship.class";
 
 export function validateQueryDescriptor(inputDescriptor: InputDescriptor, queryDescriptor: QueryDescriptor): boolean {
     return true;
