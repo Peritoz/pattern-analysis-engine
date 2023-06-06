a7adcb49b60587369db6984924cf053d3c9f2a07
Diorbert Pereira <diorbert@gmail.com>
2023-06-06 19:44:02 -0400

    refactor: refactor OutputFactory to functions

--- src/libs/engine/query_engine/output_factory.class.ts
+++ src/libs/engine/query_engine/output_factory.class.ts
@@ -2,45 +2,45 @@ import { Direction } from '@libs/model/common/enums/direction.enum';
 import { OutputEdge } from '@libs/model/output/output_edge.interface';
 import { OutputVertex } from '@libs/model/output/output_vertex.interface';
 
-export class OutputFactory {
-  static createOutputVertex(
-    id: string,
-    name: string,
-    types: Array<string>,
-    shouldBeReturned = true,
-  ): OutputVertex {
+const createOutputVertex = (
+  id: string,
+  name: string,
+  types: Array<string>,
+  shouldBeReturned = true,
+): OutputVertex => {
+  return {
+    identifier: id,
+    label: name,
+    types,
+    shouldBeReturned,
+  };
+};
+
+const createOutputEdge = (
+  direction: Direction,
+  types: Array<string>,
+  identifier?: string,
+  derivationPath?: Array<string>,
+): OutputEdge => {
+  if (Array.isArray(derivationPath) && derivationPath.length > 0) {
     return {
-      identifier: id,
-      label: name,
+      identifier,
+      direction,
       types,
-      shouldBeReturned,
+      derivationPath,
+    };
+  } else if (identifier) {
+    return {
+      identifier,
+      direction,
+      types,
+    };
+  } else {
+    return {
+      direction,
+      types,
     };
   }
+};
 
-  static createOutputEdge(
-    direction: Direction,
-    types: Array<string>,
-    identifier?: string,
-    derivationPath?: Array<string>,
-  ): OutputEdge {
-    if (Array.isArray(derivationPath) && derivationPath.length > 0) {
-      return {
-        identifier,
-        direction,
-        types,
-        derivationPath,
-      };
-    } else if (identifier) {
-      return {
-        identifier,
-        direction,
-        types,
-      };
-    } else {
-      return {
-        direction,
-        types,
-      };
-    }
-  }
-}
+export { createOutputVertex, createOutputEdge };
