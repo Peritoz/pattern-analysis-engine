55abca09576fee19afbad3c7b476a42ed6cce748
Diorbert Pereira <diorbert@gmail.com>
2026-07-27 08:24:18 -0400

    chore: update to ohm-js 17

--- src/libs/engine/query_interpreter/ohm_interpreter/ohm_interpreter.ts
+++ src/libs/engine/query_interpreter/ohm_interpreter/ohm_interpreter.ts
@@ -1,9 +1,9 @@
-import ohm, { ActionDict } from 'ohm-js';
-import { InputDescriptor } from '@libs/model/input_descriptor/input_descriptor.class';
+import {ActionDict, grammar} from 'ohm-js';
+import {InputDescriptor} from '@libs/model/input_descriptor/input_descriptor.class';
 import generateAmaqlSemantics from './semantics/semantics';
-import { getGrammar } from '@libs/engine/query_interpreter/ohm_interpreter/grammar/getGrammar';
+import {getGrammar} from '@libs/engine/query_interpreter/ohm_interpreter/grammar/getGrammar';
 
-const amaqlGrammar = ohm.grammar(getGrammar());
+const amaqlGrammar = grammar(getGrammar());
 
 const mountInputDescriptor = (query: string): InputDescriptor => {
   const semantics = amaqlGrammar
